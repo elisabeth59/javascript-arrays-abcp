03dc1c13688f022bce9dabbea3ef35f507137912
Elisabeth Narine <elisabethnarine@gmail.com>
2018-03-01 18:42:52 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -34,5 +34,6 @@ function removeElementFromEndOfArray(array) {
   return array.slice(0, array.length - 1)
 }
 function destructivelyRemoveElementFromBeginningOfArray(array) {
-  
+array.shift()
+array
 }
