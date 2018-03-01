e12a55cc8613870e3b36c89f47f79923c7046060
Elisabeth Narine <elisabethnarine@gmail.com>
2018-03-01 18:20:32 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -6,4 +6,5 @@ function addElementToBeginningOfArray(array, element) {
   return [element,...array];
 }
 function descructivelyAddElementToBeginningOfArray(array, element) {
-  array.unshift(element)
+  array.unshift(element);
+  
